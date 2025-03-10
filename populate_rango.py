c150c1dac11d36c68feeeb1f1a84a4aa5ae1fbc7
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-03-10 15:14:19 +0000

    Chapter 8 (with tests passing)

--- populate_rango.py
+++ populate_rango.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 import django
 
